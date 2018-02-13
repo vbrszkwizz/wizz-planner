d074e8a542a16d6996a81c90a0ba591863a80a14
Viktor Borszeki <vborszeki@gmail.com>
2018-02-13 20:14:18 +0100

    add form validation

--- src/SearchField/SearchField.js
+++ src/SearchField/SearchField.js
@@ -3,6 +3,7 @@ import pick from 'lodash.pick';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import DatePicker from 'material-ui/DatePicker';
+import RaisedButton from 'material-ui/RaisedButton';
 import './searchField.css';
 
 class SearchField extends Component {
@@ -15,13 +16,18 @@ class SearchField extends Component {
         : [],
       destination: localStorage.getItem('destination') || '',
       outboundDate: null,
-      inboundDate: null
+      inboundDate: null,
+      validationErrorOrigin: '',
+      validationErrorDestination: '',
+      validationErrorOutboundDate: '',
+      flights: []
     };
     this.handleOriginChange = this.handleOriginChange.bind(this);
     this.handleDestinationChange = this.handleDestinationChange.bind(this);
     this.handleOutboundDateChange = this.handleOutboundDateChange.bind(this);
     this.handleInboundDateChange = this.handleInboundDateChange.bind(this);
     this.disableDates = this.disableDates.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleOriginChange(event) {
@@ -32,20 +38,90 @@ class SearchField extends Component {
 
     localStorage.setItem('origin', origin);
     this.setState({ origin }, () => this.calculateConnections());
+
+    this.setState({ validationErrorOrigin: '' });
   }
 
   handleDestinationChange(event) {
     let destination = event.target.innerHTML;
     localStorage.setItem('destination', destination);
     this.setState({ destination });
+    this.setState({ validationErrorDestination: '' });
   }
 
   handleOutboundDateChange(event, date) {
     this.setState({ outboundDate: date });
+    this.setState({ validationErrorOutboundDate: '' });
   }
 
   handleInboundDateChange(event, date) {
-    this.setState({ inboundDate: date })
+    this.setState({ inboundDate: date });
+  }
+
+  handleSubmit() {
+    let payload = {
+      origin: this.getIataCode(this.props.stations, this.state.origin),
+      destination: this.getIataCode(
+        this.props.stations,
+        this.state.destination
+      ),
+      outboundDate: this.formatDate(this.state.outboundDate),
+      inboundDate: this.formatDate(this.state.inboundDate)
+    };
+
+    this.validateForm();
+
+    if (
+      this.state.origin === '' ||
+      this.state.destination === '' ||
+      this.state.outboundDate === null
+    ) {
+      return;
+    }
+
+    fetch(
+      `https://mock-air.herokuapp.com/search?departureStation=${
+        payload.origin
+      }&arrivalStation=${payload.destination}&date=${payload.outboundDate}`
+    )
+      .then(res => res.json())
+      .then(flights => this.setState({ flights }))
+      .catch(console.error);
+  }
+
+  validateForm() {
+    if (this.state.origin === '') {
+      this.setState({ validationErrorOrigin: 'departure is required' });
+    } else {
+      this.setState({ validationErrorOrigin: '' });
+    }
+    if (this.state.destination === '') {
+      this.setState({ validationErrorDestination: 'destination is required' });
+    } else {
+      this.setState({ validationErrorDestination: '' });
+    }
+    if (this.state.outboundDate === null) {
+      this.setState({
+        validationErrorOutboundDate: 'departure date is required'
+      });
+    } else {
+      this.setState({ validationErrorOutboundDate: '' });
+    }
+  }
+
+  formatDate(date) {
+    return date === null
+      ? null
+      : `${date.getUTCFullYear()}-${(date.getMonth() + 1)
+          .toString()
+          .padStart(2, '0')}-${date
+          .getDate()
+          .toString()
+          .padStart(2, '0')}`;
+  }
+
+  getIataCode(stations, city) {
+    return city === '' ? '' : stations.find(e => e.shortName === city).iata;
   }
 
   calculateConnections() {
@@ -86,6 +162,10 @@ class SearchField extends Component {
         return 0;
       });
 
+    const style = {
+      margin: 12
+    };
+
     return (
       <form>
         <SelectField
@@ -93,6 +173,7 @@ class SearchField extends Component {
           floatingLabelText="from"
           value={this.state.origin}
           onChange={this.handleOriginChange}
+          errorText={this.state.validationErrorOrigin}
         >
           {stations.map(station => (
             <MenuItem
@@ -107,6 +188,7 @@ class SearchField extends Component {
           floatingLabelText="to"
           value={this.state.destination}
           onChange={this.handleDestinationChange}
+          errorText={this.state.validationErrorDestination}
         >
           {this.state.connections.map(connection => (
             <MenuItem
@@ -121,6 +203,8 @@ class SearchField extends Component {
             hintText="departure"
             value={this.state.outboundDate}
             onChange={this.handleOutboundDateChange}
+            minDate={new Date()}
+            errorText={this.state.validationErrorOutboundDate}
           />
           <DatePicker
             hintText="return"
@@ -129,6 +213,11 @@ class SearchField extends Component {
             shouldDisableDate={this.disableDates}
           />
         </div>
+        <RaisedButton
+          label="search flights"
+          style={style}
+          onClick={this.handleSubmit}
+        />
       </form>
     );
   }
